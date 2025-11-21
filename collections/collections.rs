a0aed8d4417181d415ddee7a4ab4959dde3ee9ab
Keran Yang <yangkr920208@gmail.com>
2025-11-21 17:44:46 -0500

    chore: vectors
    Signed-off-by: Keran Yang <yangkr920208@gmail.com>

---
+++ collections/collections.rs
@@ -0,0 +1,90 @@
+fn main() {
+    vector_collections_with_stack_data();
+    vector_collections_with_heap_data();
+    vector_use_enum_to_store_different_types();
+    questions_about_vector();
+}
+
+fn vector_collections_with_stack_data() {
+    // basic syntax
+    // creation
+    let mut v: Vec<i32> = Vec::new(); // standard way
+    let mut v1 = vec![1, 2, 3]; // macro way
+    // adding elements
+    v.push(10);
+    v.push(20);
+    v.push(30);
+    // accessing elements
+    // v1[0] is a reference to the element at index 0, it's type is &i32
+    let first = v1[0]; // panics if out of bounds
+    let second = v1.get(1); // returns Option<&T>
+    // iterating
+    // immutable iteration
+    for i in &v1 {
+        println!("{}", i);
+    }
+    // mutable iteration
+    for i in &mut v {
+        *i += 1;
+    }
+    // iterator methods
+    let mut v_ref = &mut v1;
+    for num_ref in v_ref.iter() {
+        // v_ref.push(*num_ref); // this won't compile because we are trying to mutate v_ref while iterating over it
+    }
+}
+
+fn vector_collections_with_heap_data() {
+    // vector of Strings
+    let mut v: Vec<String> = vec![
+        String::from("a"),
+        String::from("b"),
+        String::from("c"),
+    ];
+    // v[0] is a reference to the String at index 0, it's type is &String
+    // since the vector is declared as mutable, we can mutate the String inside the vector
+    // it's an in-place mutation, so the ownership of the String does not change.
+    v[0].push('1');
+    println!("{}", v[0]);
+
+    let v1: Vec<String> = vec![
+        String::from("x"),
+        String::from("y"),
+        String::from("z"),
+    ];
+    // let s: String = v1[0]; // this line causes a compile error because we are trying to move the ownership of the String out of the vector
+    // Note: non-copyable types cannot be moved out of a vector by indexing.
+}
+
+fn vector_use_enum_to_store_different_types() {
+    enum SpreadsheetCell {
+        Int(i32),
+        Float(f64),
+        Text(String),
+    }
+    let row: Vec<SpreadsheetCell> = vec![
+        SpreadsheetCell::Int(10),
+        SpreadsheetCell::Float(20.5),
+        SpreadsheetCell::Text(String::from("Hello")),
+    ];
+}
+
+fn questions_about_vector() {
+    /*
+    Determine whether the program will pass the compiler. If it passes, write the expected output of the program if it were executed.
+
+    fn main() {
+      let mut v: Vec<i32> = vec![1, 2, 3];
+      let mut v2: Vec<&mut i32> = Vec::new();
+      for i in &mut v {
+        v2.push(i);
+      }
+      *v2[0] = 5;
+      let a = *v2[0];
+      let b = v[0];
+      println!("{a} {b}");
+    }
+     */
+    // Answer: the program will pass. The expected output is "5 5".
+    // The key point here is that v2 is a vector of **mutable references** to the elements in v.
+}

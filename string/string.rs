5f9dbf7a751d5bc101092b580cc774eba246a297
Keran Yang <yangkr920208@gmail.com>
2025-11-22 21:38:36 -0500

    chore: more string
    Signed-off-by: Keran Yang <yangkr920208@gmail.com>

--- string/string.rs
+++ string/string.rs
@@ -17,11 +17,16 @@ fn main() {
 
   // Note: string literal is &str type
   // string literals are immutable references since &str is an immutable string slice.
-  let str_literal: &str = "Hello, world!";
+  let _str_literal: &str = "Hello, world!";
+
+  all_about_string();
 
   size_of_types();
 
   questions_about_string();
+
+  // Note: last but not least, String doesn't allow indexing because it's not clear what an index would mean in the context of UTF-8 encoded strings.
+  // Is it a byte index, a character index, or a grapheme cluster index?
 }
 
 fn string_str_and_slice() {
@@ -36,6 +41,40 @@ fn string_str_and_slice() {
   println!("String: {}", s);
   println!("&String: {}", string_ref);
   println!("&str: {}", str_slice);
+}
+
+fn all_about_string() {
+  // Two ways of initializing a String
+  let _ss1 = String::from("abc");
+  let _ss2 = "def".to_string();
+
+  // The push_str method appends a string slice to the String.
+  // It does not take ownership of the string slice.
+  let mut s1 = String::from("Hello, ");
+  let s2 = String::from("world!");
+  s1.push_str(&s2);
+  println!("can print s because s1 still owns the data: {}", s1);
+  println!("can print s2 because s2 is not moved: {}", s2);
+
+  // Another way of appending is using the + operator.
+  // The difference is that the + operator takes ownership of the left operand and returns a new String.
+  let s3 = String::from("Goodbye, ");
+  let s4 = "world!";
+  let s5 = s3 + "world!"; // s3 is moved here and can no longer be used
+  // behind the scenes,
+  // the + operator uses the add method which takes ownership of the left operand (s3) and borrows the right operand (&str).
+  // `fn add(self, s: &str) -> String {}`
+  // println!("s3: {}", s3); // This line would cause a compile error
+  println!("can print s4 because s4 is a &str and not moved: {}", s4);
+  println!("s5: {}", s5);
+
+  // Another way to concatenate strings is using the format! macro.
+  let s6 = String::from("tic");
+  let s7 = String::from("tac");
+  let s8 = String::from("toe");
+  let s9 = format!("{}-{}-{}", s6, s7, s8);
+  // Note: format! does not take ownership of any of its arguments.
+  println!("s9: {}", s9);
 }
 
 fn size_of_types() {
@@ -75,5 +114,19 @@ fn questions_about_string() {
   Answer: &str is better than String when:
   1. You want to **avoid heap allocation** for small or temporary strings.
   2. You want to work with string literals or borrowed string data **without taking ownership**.
+   */
+
+  /*
+  Question: What is the maximum number of times a heap allocation could occur in this program? Write your answer in digits, e.g. 0 or 1.
+  ```
+  let s1 = String::from("tic");
+  let s2 = String::from("tac");
+  let s3 = String::from("toe");
+  let s = s1 + "-" + &s2 + "-" + &s3;
+  ```
+  Answer: 7.
+  The first 3 allocations are from the String::from calls for s1, s2, and s3.
+  The next 4 allocations occur during the concatenation process, one for each + operation.
+  For each + operation, if the allocated capacity is insufficient to hold the new combined string, a new allocation will be made.
    */
 }
